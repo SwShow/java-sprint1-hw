bbe9435fddfd097d69d4c4bf2e819c200ddc5e08
Svetlana <swetlanakorowina@yandex.ru>
2022-04-06 00:20:39 +0300

    Class Converter

---
+++ src/com/company/Converter.java
@@ -0,0 +1,13 @@
+package com.company;
+
+  class Converter {
+
+     public static void regDistance(int sumSteps) {
+         int kilometres = sumSteps * 75 / 100000;
+         System.out.println("Вы прошли: " + kilometres + " километров");
+     }
+     public static void regKal(int sumSteps) {
+         int energy = sumSteps * 50 / 1000;
+         System.out.println("Вы потратили: " + energy + " килокалорий!");
+     }
+}
