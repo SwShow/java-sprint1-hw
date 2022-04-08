b2a3531d787b9844070bd1c53b40fecbf004f4f0
Svetlana <swetlanakorowina@yandex.ru>
2022-04-09 01:02:18 +0300

    Исправлены ошибки и замечания, улучшена реализация массива

--- src/com/company/StepTracker.java
+++ src/com/company/StepTracker.java
@@ -6,7 +6,7 @@ public class StepTracker {
 
 
     // Данные о шагах за определенный месяц,    массив создан наоборот для удобства пользователя
-    static int[][] fullStep2D = new int[31][13];    // что лучше: зарезервировать лишнюю ячейку или
+    static int[][] fullStep2D = new int[30][12];    // что лучше: зарезервировать лишнюю ячейку или
 
     // производить вычисления в нескольких методах ?
     // Изменение целевого количества шагов
@@ -27,38 +27,41 @@ public class StepTracker {
         Scanner scan = new Scanner(System.in);    // если вынести из метода, не работает
         System.out.println("Какое сегодня число?");
         int day = scan.nextInt();
-        while (day < 1 || day > 31) {
+        while (day < 1 || day > 30) {
             askOtherNumber();
             day = scan.nextInt();
         }
         System.out.println("Какой сейчас месяц?");
         int month = scan.nextInt();
-        while (month < 1 || month > 13) {
+        while (month < 1 || month > 12) {
             askOtherNumber();
             month = scan.nextInt();
 
         }
 
         System.out.println("Введите количество пройденных шагов");
-        fullStep2D[day][month] = scan.nextInt();
-        if (fullStep2D[day][month] < 0) {
+        int i = day - 1;
+        int j = month - 1;
+        fullStep2D[i][j] = scan.nextInt();
+        if (fullStep2D[i][j] < 0) {
             askOtherNumber();
         }
-        System.out.println(day + "." + month + "." + " шагов: " + fullStep2D[day][month]);
+        System.out.println(day + "." + month + "." + " шагов: " + fullStep2D[i][j]);
     }
 
     // Вывод статистики:
     static void printStatistic(int month) {
+        int j = month - 1;
         // количество шагов по дням
-        for (int i = 1; i < fullStep2D.length; i++) {
-            System.out.print("День " + i + ": " + fullStep2D[i][month] + ", ");
+        for (int i = 0; i < fullStep2D.length; i++) {
+            System.out.print("День " + (i + 1) + ": " + fullStep2D[i][j] + ", ");
         }
         System.out.print("\n");
 
         // общее количество шагов за месяц
         int sumSteps = 0;
-        for (int i = 1; i < fullStep2D.length; i++) {
-            sumSteps = sumSteps + fullStep2D[i][month];
+        for (int i = 0; i < fullStep2D.length; i++) {
+            sumSteps = sumSteps + fullStep2D[i][j];
         }
         System.out.println("За месяц вы прошли: " + sumSteps + " шагов.");
 
@@ -68,7 +71,7 @@ public class StepTracker {
         System.out.println("Максимальное количество шагов: " + max);
 
         // среднее количество шагов
-        System.out.println("Среднее количество шагов: " + (sumSteps / 30));
+        System.out.println("Среднее количество шагов: " + (sumSteps / fullStep2D.length));
 
         // пройденная дистанция
         Converter.regDistance(sumSteps);
@@ -80,9 +83,10 @@ public class StepTracker {
     // лучшая серия: максимальное количество подряд идущих дней когда шагов за день было равно или выше целевого
     static void getBestCount(int stepDay, int month) {
         int k = 0;
+        int j = month - 1;
         int finishBestCount = 0;
-        for (int i = 1; i < fullStep2D.length; i++) {
-            if (fullStep2D[i][month] >= stepDay) {
+        for (int i = 0; i < fullStep2D.length; i++) {
+            if (fullStep2D[i][j] >= stepDay) {
                 k++;
             } else if (k > finishBestCount) {
                 finishBestCount = k;
@@ -95,9 +99,10 @@ public class StepTracker {
 
     // Максимальное кол шагов в месяце
     public static int getMaxStep(int month, int maxStep) {
-        for (int i = 1; i < 31; i++) {
-            if (fullStep2D[i][month] > maxStep) {
-                maxStep = fullStep2D[i][month];
+        int j = month - 1;
+        for (int i = 0; i < fullStep2D.length; i++) {
+            if (fullStep2D[i][j] > maxStep) {
+                maxStep = fullStep2D[i][j];
             }
         }
         return maxStep;
